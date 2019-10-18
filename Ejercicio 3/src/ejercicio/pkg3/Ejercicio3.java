c31885f2c1229b7b9fe2b6b68ffcb4eb0625eff2
Fede cardozo <fedecardozo.a@gmail.com}>
2019-10-17 22:22:50 -0300

    Ejercicio 3

---
+++ Ejercicio 3/src/ejercicio/pkg3/Ejercicio3.java
@@ -0,0 +1,22 @@
+package ejercicio.pkg3;
+import java.util.Scanner;
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+        
+        int num1,num2;
+        double cociente;
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Ingrese 2 numero: ");
+        num1 = entrada.nextInt();
+        num2 = entrada.nextInt();
+        
+        cociente = (double)num1/num2;
+        
+        System.out.println("El cociente es: " + cociente);
+        
+    }
+    
+}
