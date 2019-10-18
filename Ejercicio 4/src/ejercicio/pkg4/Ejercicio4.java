54019684aa342c86cf8480303d91be64cc7834ec
Fede cardozo <fedecardozo.a@gmail.com}>
2019-10-18 18:19:16 -0300

    Ejercicio 4

--- Ejercicio 4/src/ejercicio/pkg4/Ejercicio4.java
+++ Ejercicio 4/src/ejercicio/pkg4/Ejercicio4.java
@@ -1,10 +1,22 @@
 package ejercicio.pkg4;
 
+import java.util.Scanner;
+
 public class Ejercicio4 {
 
     public static void main(String[] args) {
         
+        final double PI = 3.14;
+        int diametro;
+        int perimetro;
         
+        System.out.println("Ingrese diametro del circulo: ");
+        Scanner entrada = new Scanner(System.in);
+        diametro = entrada.nextInt();
+        
+        perimetro = (int) (diametro * PI);
+        
+        System.out.println("El perimetro es: " + perimetro);
         
     }
     
